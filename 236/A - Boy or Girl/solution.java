2272db8360f4ce6304fd5699bb1888a2340084c8
Aryawart-Gupta <aryawart.24bce11138@vitbhopal.ac.in>
2026-08-08 12:19:35 +0530

    Add Boy or Girl [A] from Codeforces

---
+++ 236/A - Boy or Girl/solution.java
@@ -0,0 +1,23 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String username = scanner.next();
+ 
+        Set<Character> distinctChars = new HashSet<>();
+        for (char c : username.toCharArray()) {
+            distinctChars.add(c);
+        }
+ 
+        if (distinctChars.size() % 2 == 0) {
+            System.out.println("CHAT WITH HER!");
+        } else {
+            System.out.println("IGNORE HIM!");
+        }
+ 
+        scanner.close();
+    }
+}
